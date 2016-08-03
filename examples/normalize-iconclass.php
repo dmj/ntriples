1e52c29f612c0f4634bbbb7a413e4c1897c513c5
David Maus <maus@hab.de>
2016-08-03 11:42:22 +0200

    Add example whitespace normalization

---
+++ examples/normalize-iconclass.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * Normalize whitespaces in Iconclass classification codes.
+ *
+ * @see http://www.iconclass.nl/contents-of-iconclass
+ *
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+function normalize_whitespace ($value) {
+    $len = strlen($value);
+    $keep = false;
+    $normalized = '';
+    for ($i = 0; $i < $len; $i++) {
+        $octet = $value[$i];
+        if ($octet === '(') {
+            $keep = true;
+        }
+        if ($octet === ')') {
+            $keep = false;
+        }
+        if ($octet !== ' ' or $keep === true) {
+            $normalized .= $octet;
+        }
+    }
+    return $normalized;
+}
+
+$reader = new HAB\NTriples\Reader\Reader();
+$serializer = new HAB\NTriples\Serializer\NTriples();
+
+$outfile = fopen('php://stdout', 'wb');
+
+$reader->open('iconclass.nt');
+$count = 0;
+while ($triple = $reader->read()) {
+    list($s, $p, $o) = $triple;
+    if ($o instanceof HAB\NTriples\Literal and $o->getDatatype() === 'http://uri.hab.de/ontology/diglib-types#Iconclass') {
+        $o = new HAB\NTriples\Literal(normalize_whitespace($o->getValue()), null, $o->getDatatype());
+    }
+    fwrite($outfile, $serializer->serializeTriple($s, $p, $o));
+    fwrite($outfile, PHP_EOL);
+}
+$reader->close();
+
+fclose($outfile);
